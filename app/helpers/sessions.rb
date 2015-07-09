cffe9b58ce14ee949e039323ef5b9c9e3a7dcaea
Tim Cannady <timcannady@gmail.com>
2015-07-09 15:44:40 -0700

    add framework for landing page, sign up, login, logout. Hasn't been tested due to no DB yet

--- app/helpers/sessions.rb
+++ app/helpers/sessions.rb
@@ -1,4 +1,20 @@
+helpers do
 
+  def sign_up
+    user = User.new(email: params[:email])
+    user.password = params[:password]
+    user.save!
+  end
 
+  def login
+    user = User.where(email: params[:email]).first
+    if user && user.password == params[:password]
+      sessions[:id] = params[:id]
+    end
+  end
 
-sessions.rb
+  def logout
+    sessions[:id] = nil
+  end
+
+end
