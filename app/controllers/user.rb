fe8afa4a65125742d733156ea18010b267c63569
amjacobowitz & Marisa Ramon <pair+amjacobowitz.mpieces@devbootcamp.com>
2015-07-09 16:33:21 -0700

    add header, home route and profile routes for users

---
+++ app/controllers/user.rb
@@ -0,0 +1,38 @@
+get '/users' do
+  redirect '/home'
+end
+
+
+get '/users/:id' do
+  # This shows the profile of a user
+  # if session[:user_id]
+  erb :'user/user_show'
+  # else
+  #   redirect '/login'
+  # end
+
+end
+
+get '/users/:id/followers' do
+
+end
+
+get '/users/:id/following' do
+
+end
+
+get '/home' do
+  # if session[:user_id]
+    erb :home
+  # else
+  #   redirect '/login'
+  # end
+end
+
+post '/search' do
+  redirect '/users/search_results'
+end
+
+get '/users/search_results' do
+
+end
--- app/helpers/sessions.rb
+++ app/helpers/sessions.rb
@@ -1,4 +1,1 @@
 
-
-
-sessions.rb
