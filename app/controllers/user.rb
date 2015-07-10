b024137408ec4ba1e0139b977f5a9d2832a0ffb1
amjacobowitz & Nicholas Toulouse <pair+amjacobowitz.nicoto@devbootcamp.com>
2015-07-10 15:24:47 -0700

    able to follow and unfollow

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -25,30 +25,29 @@ get '/users/:id' do
   @current_profile_id = params[:id].to_i
   # This shows the profile of a user
   if session[:user_id]
-    if session[:user_id] == params[:id].to_i
-      @users_tweets = current_user.tweets
-      users_following = current_user.leaders
-      users_following_tweets = []
+    @users_tweets = current_user.tweets
+    users_following = current_user.leaders
+    users_following_tweets = []
 
-      users_following.each do |user|
-        users_following_tweets << user.tweets
-      end
-      @user_and_following_tweets = users_following_tweets + @users_tweets
-      erb :'user/user_show'
-    else
-      redirect '/'
+    users_following.each do |user|
+      users_following_tweets << user.tweets
     end
+    p '###########################'
+    @user_and_following_tweets = users_following_tweets + @users_tweets
+    erb :'user/user_show'
+  else
+    redirect '/'
   end
 end
 
 delete '/users/:id' do
-  UserRelationship.destroy(minion_id: params[:id])
-  redirect '/users/#{params[:id]}'
+  UserRelationship.find_by(leader_id: params[:id], minion_id: current_user.id).destroy
+  redirect "/users/#{params[:id]}"
 end
 
 post '/users/:id' do
   UserRelationship.create!(leader_id: params[:id], minion_id: current_user.id)
-  redirect '/users/#{params[:id]}'
+  redirect "/users/#{params[:id]}"
 end
 
 get '/users/:id/followers' do
