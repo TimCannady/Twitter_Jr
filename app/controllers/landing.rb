d3afafaa09672bc8894667a99172a447a4c5928f
Andrew Donato <andrew.donato91@gmail.com>
2015-07-09 17:33:42 -0700

    pulled

--- app/controllers/landing.rb
+++ app/controllers/landing.rb
@@ -1,27 +1,4 @@
 get '/' do
-  erb :'landing'
+  erb :landing, :layout => false
 end
 
-post '/users/new' do
-  sign_up
-  login
-  redirect "/home"
-end
-
-#/login or /users/login?
-post '/login' do
-  login
-  redirect "/home"
-end
-
-# post or get?
-post '/logout' do
-  logout
-end
-
-
-# have users var as the new user
-# query the database by email
-# users.last.email == user.email
-#
-# if user && user.
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,29 @@
 class User < ActiveRecord::Base
+  include BCrypt
 
   has_many :tweets
   has_many :leader_relationships, class_name: 'UserRelationship', foreign_key: 'minion_id'
-  has_many :leaders, through: :leader_relationships, source: :leader
+  has_many :leaders, through: :minion_relationships, source: :minion
 
   has_many :minion_relationships, class_name: 'UserRelationship', foreign_key: 'leader_id'
-  has_many :minions, through: :minion_relationships, source: :minion
+  has_many :minions, through: :leader_relationships, source: :leader
 
-  has_many :minion_tweets, through: :minions, source: :tweets
+  # has_many :minion_tweets, through: :minions, source: :tweets
 
   validates :username, presence: true, uniqueness: true
   validates :password_hash, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
 
