6ac5564fd4412643e344a0659dc7c64fb37fc695
amjacobowitz & Nicholas Toulouse <pair+amjacobowitz.nicoto@devbootcamp.com>
2015-07-10 14:52:25 -0700

    following count correct

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -25,10 +25,7 @@ get '/users/:id' do
   @current_profile_id = params[:id].to_i
   # This shows the profile of a user
   if session[:user_id]
-    erb :'user/user_show'
-
     if session[:user_id] == params[:id].to_i
-
       @users_tweets = current_user.tweets
       users_following = current_user.leaders
       users_following_tweets = []
@@ -36,10 +33,8 @@ get '/users/:id' do
       users_following.each do |user|
         users_following_tweets << user.tweets
       end
-
       @user_and_following_tweets = users_following_tweets + @users_tweets
-     erb :'user/user_show'
-
+      erb :'user/user_show'
     else
       redirect '/'
     end
